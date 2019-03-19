4daafa1e62e160a2cd9642bcde13a6cf15a9e8eb
Kimberly Aviles <01010110.v@gmail.com>
2019-03-19 04:58:05 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ class Doctor
   end
 
   def patients
-    @patients = nil
+    @patients = []
     self.appointments.collect do |each_appointment|
       @patients << each_appointment.patient
     end
